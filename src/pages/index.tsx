bdb905190b4692092345dee0659069ab0074a41a
kimiya98901 <jojar_kga@hotmail.com>
2022-08-23 12:53:19 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,9 +93,9 @@ const MovieList = (props: MovieListProps) => {
   return (
     <ul
       role="list"
-      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mb-6"
     >
-      {movies.map((movie) => (
+      {movies.map((movie, index) => (
         <li key={movie.title} className="relative">
           <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
             <Image
@@ -113,7 +113,7 @@ const MovieList = (props: MovieListProps) => {
             </button>
           </div>
           <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
-            {movie.title}
+            {index + 1}. {movie.title}
           </p>
           <p className="block text-sm font-medium text-gray-500 pointer-events-none">
             {movie.popularity}
@@ -168,21 +168,23 @@ const Home: NextPage = () => {
       <div className="relative bg-gray-50 overflow-hidden min-h-screen">
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
-            <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
+            <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight mb-6">
               <span className="block xl:inline">themovie</span>
               <span className="block text-indigo-600 xl:inline">db</span>
             </h1>
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Tryck på knappen för att hämta de populäraste 100 filmerna just
-              nu.
-            </p>
             {movies?.length === 0 ? (
-              <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-                <FetchMovieButton
-                  setMovies={setMovies}
-                  fetchMovies={fetchMovies}
-                />
-              </div>
+              <>
+                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                  Tryck på knappen för att hämta de populäraste 100 filmerna
+                  just nu.
+                </p>
+                <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+                  <FetchMovieButton
+                    setMovies={setMovies}
+                    fetchMovies={fetchMovies}
+                  />
+                </div>
+              </>
             ) : (
               <MovieList movies={movies} />
             )}
